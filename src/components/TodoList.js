73357044e6154fd4423067c03bc33c7405064e55
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2018-12-18 04:23:36 +0400

    add todolist

---
+++ src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+export default class TodoList extends React.Component {
+  state = {
+    todos: []
+  };
+
+  addTodo = todo => {
+    this.setState({
+      todos: [todo, ...this.state.todos]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} text={todo.text} />
+        ))}
+      </div>
+    );
+  }
+}
